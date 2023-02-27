607a8f8cf7991dbe4c5d9db25654d30af7101e49
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-27 14:29:56 +0100

    added do while, declared var

--- Program.cs
+++ Program.cs
@@ -5,4 +5,31 @@
  * Do this until the user types "exit".
  * 
  */
+bool exitStatus = false;
+string firstName;
+string inputStatus;
 
+
+do
+{
+    Console.Write("What is your first name?: ");
+    firstName = Console.ReadLine();
+
+    switch (firstName.ToLower())
+    {
+        case "josh":
+            Console.WriteLine("Welcome back, Professor!");
+            break;
+        default:
+            Console.WriteLine("Welcome, Student!");
+            break;
+    }
+
+    Console.WriteLine("Type Exit to close, or press ENTER to continue");
+    inputStatus = Console.ReadLine();
+    if (inputStatus.ToLower() == "exit")
+    {
+        exitStatus = true;
+    }
+
+} while (exitStatus == false);
